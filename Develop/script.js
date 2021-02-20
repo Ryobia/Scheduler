d059d2a29b40e1e70bd7d317d4b75d0676c26357
Ryobia <ryobia36@gmail.com>
2021-02-20 12:19:38 -0700

    added color changes based on current time, it's finished!

--- Develop/script.js
+++ Develop/script.js
@@ -108,37 +108,120 @@ setInterval(function() {
     let h = d.getHours();
     console.log(h)
 
-    if(h = 2) {
+
+    // Right after finishing all of this copying and pasting I figured out a much more efficient way to do it but the work was already done so I'm leaving it as is in all it's glory
+    if(h === 9) {
         $('#hourNine').addClass('present');
+        $('#hourTen').addClass('future');
+        $('#hourEleven').addClass('future');
+        $('#hourTwelve').addClass('future');
+        $('#hourThirteen').addClass('future');
+        $('#hourFourteen').addClass('future');
+        $('#hourFifteen').addClass('future');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 10) {
+    } else if(h === 10) {
+        $('#hourNine').addClass('past');
         $('#hourTen').addClass('present');
+        $('#hourEleven').addClass('future');
+        $('#hourTwelve').addClass('future');
+        $('#hourThirteen').addClass('future');
+        $('#hourFourteen').addClass('future');
+        $('#hourFifteen').addClass('future');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 11) {
+    } else if(h === 11) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
         $('#hourEleven').addClass('present');
+        $('#hourTwelve').addClass('future');
+        $('#hourThirteen').addClass('future');
+        $('#hourFourteen').addClass('future');
+        $('#hourFifteen').addClass('future');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 12) {
+    } else if(h === 12) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
         $('#hourTwelve').addClass('present');
+        $('#hourThirteen').addClass('future');
+        $('#hourFourteen').addClass('future');
+        $('#hourFifteen').addClass('future');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 13) {
+    } else if(h === 13) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
+        $('#hourTwelve').addClass('past');
         $('#hourThirteen').addClass('present');
+        $('#hourFourteen').addClass('future');
+        $('#hourFifteen').addClass('future');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 14) {
+    } else if(h === 14) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
+        $('#hourTwelve').addClass('past');
+        $('#hourThirteen').addClass('past');
         $('#hourFourteen').addClass('present');
+        $('#hourFifteen').addClass('future');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 15) {
+    } else if(h === 15) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
+        $('#hourTwelve').addClass('past');
+        $('#hourThirteen').addClass('past');
+        $('#hourFourteen').addClass('past');
         $('#hourFifteen').addClass('present');
+        $('#hourSixteen').addClass('future');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 16) {
+    } else if(h === 16) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
+        $('#hourTwelve').addClass('past');
+        $('#hourThirteen').addClass('past');
+        $('#hourFourteen').addClass('past');
+        $('#hourFifteen').addClass('past');
         $('#hourSixteen').addClass('present');
+        $('#hourSeventeen').addClass('future');
 
-    } else if(h = 17) {
+    } else if(h === 17) {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
+        $('#hourTwelve').addClass('past');
+        $('#hourThirteen').addClass('past');
+        $('#hourFourteen').addClass('past');
+        $('#hourFifteen').addClass('past');
+        $('#hourSixteen').addClass('past');
         $('#hourSeventeen').addClass('present');
 
     } else {
+        $('#hourNine').addClass('past');
+        $('#hourTen').addClass('past');
+        $('#hourEleven').addClass('past');
+        $('#hourTwelve').addClass('past');
+        $('#hourThirteen').addClass('past');
+        $('#hourFourteen').addClass('past');
+        $('#hourFifteen').addClass('past');
+        $('#hourSixteen').addClass('past');
+        $('#hourSeventeen').addClass('past');
 
     }
-}, 1000);
+}, 2000);
     
     
 
