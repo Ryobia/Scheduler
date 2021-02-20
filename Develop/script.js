d84608f32f462abab7b6a7b525e68a92359308d6
Ryobia <ryobia36@gmail.com>
2021-02-19 21:12:11 -0700

    added local storage functionality, can save changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
-let hours = {};
+let hours = JSON.parse(localStorage.getItem("hours")) || {
+    nine: [],
+    ten: [],
+    eleven: [],
+    twelve: [],
+    thirteen: [],
+    fourteen: [],
+    fifteen: [],
+    sixteen: [],
+    seventeen: []
+};
 let m = moment();
 
 let saveHours = function() {
@@ -6,23 +16,93 @@ let saveHours = function() {
 };
 
 let loadHours = function() {
-    hours = JSON.parse(localStorage.getItem("hours"));
-
-    if(!hours) {
-        hours = {
-            nine: [],
-            ten: [],
-            eleven: [],
-            twelve: [],
-            thirteen: [],
-            fourteen: [],
-            fifteen: [],
-            sixteen: [],
-            seventeen: []
-        };
-    }
-
+    console.log(hours);
+    $("#hourNine").html(hours.nine);
+    $("#hourTen").html(hours.ten);
+    $("#hourEleven").html(hours.eleven);
+    $("#hourTwelve").html(hours.twelve);
+    $("#hourThirteen").html(hours.thirteen);
+    $("#hourFourteen").html(hours.fourteen);
+    $("#hourFifteen").html(hours.fifteen);
+    $("#hourSixteen").html(hours.sixteen);
+    $("#hourSeventeen").html(hours.seventeen);
 
 };
+
+$("#btnNine").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourNine").val().trim();
+    hours.nine = text;
+
+    saveHours();
+});
+
+$("#btnTen").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourTen").val().trim();
+    hours.ten = text;
+
+    saveHours();
+});
+
+$("#btnEleven").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourEleven").val().trim();
+    hours.eleven = text;
+
+    saveHours();
+});
+
+$("#btnTwelve").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourTwelve").val().trim();
+    hours.twelve = text;
+
+    saveHours();
+});
+
+$("#btnThirteen").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourThirteen").val().trim();
+    hours.thirteen = text;
+
+    saveHours();
+});
+
+$("#btnFourteen").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourFourteen").val().trim();
+    hours.fourteen = text;
+
+    saveHours();
+});
+
+$("#btnFifteen").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourFifteen").val().trim();
+    hours.fifteen = text;
+
+    saveHours();
+});
+
+$("#btnSixteen").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourSixteen").val().trim();
+    hours.sixteen = text;
+
+    saveHours();
+});
+
+$("#btnSeventeen").on("click", function(event) {
+    event.preventDefault();
+    let text = $("#hourSeventeen").val().trim();
+    hours.seventeen = text;
+
+    saveHours();
+});
+
+
+loadHours();
+
 
 $('#currentDay').text(m.format("dddd, MMM do"));
